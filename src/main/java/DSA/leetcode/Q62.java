3006479e26ac5488afd9d839d33dbd24dd1bbc5d
ravi <ravikanthmp123@gmail.com>
2021-08-17 10:39:16 -0700

    Q62 O(N) space

--- src/main/java/DSA/leetcode/Q62.java
+++ src/main/java/DSA/leetcode/Q62.java
@@ -3,37 +3,28 @@ package DSA.leetcode;
 import java.util.Arrays;
 
 public class Q62 {
-
-    private int[][] tab;
-    private int M;
-    private int N;
-
     public int uniquePaths(int m, int n) {
-        this.M = m;
-        this.N = n;
-        tab = new int[m + 1][n + 1];
-        tab[m][n] = 1;
-        Arrays.fill(tab[M], 1, N, 1);
 
-        for (int i = M - 1; i >= 1 ; i--) {
-            for (int j = N; j >= 1 ; j--) {
-                if (i == M && j == N){
-                    tab[i][j] = 1;
-                }else {
-                    tab[i][j] = unique(i, j + 1) + unique(i + 1, j);
+        int[] tab = new int[n];
+
+        Arrays.fill(tab, 1);
+
+        for(int row = 1; row < m; row++){
+
+            int prev = 0;
+            for (int i = 0; i < n ; i++) {
+
+                if(i == 0){
+                    tab[i] = 1;
+                }else{
+                    tab[i] += prev;
                 }
-
+                prev = tab[i];
             }
         }
-        return tab[1][1];
-    }
 
-    private int unique(int i, int j){
-        if (i == 0 || j == 0 || i > M || j > N){
-            return 0;
-        }else {
-            return tab[i][j];
-        }
+
+        return tab[n-1];
     }
 
     public static void main(String[] args) {
