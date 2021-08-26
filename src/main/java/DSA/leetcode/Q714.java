34d2b97ebdcf819f12b347573dfad98a029a7fda
ravi <ravikanthmp123@gmail.com>
2021-08-26 14:45:21 -0700

    Buy/Sell stocks :)

--- src/main/java/DSA/leetcode/Q714.java
+++ src/main/java/DSA/leetcode/Q714.java
@@ -1,17 +1,22 @@
 package DSA.leetcode;
 
-import static java.lang.Math.max;
-
 public class Q714 {
 
     public int maxProfit(int[] prices, int fee) {
-        return 0;
+        int[] f = new int[prices.length];
+        f[0] = 0;
+        int runningMax = Integer.MIN_VALUE;
+        for (int i = 1; i < f.length; i++) {
+            runningMax = Math.max(runningMax, f[i - 1] - prices[i - 1]);
+            f[i] = Math.max(f[i - 1], runningMax + prices[i] - fee);
+        }
+        return f[prices.length - 1];
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 1, 4, 4, 2, 3, 2, 5, 1, 2};
+        int[] arr = {1,3};
         Q714 test = new Q714();
-        System.out.println(test.maxProfit(arr, 1));
+        System.out.println(test.maxProfit(arr, 2));
 
     }
 
