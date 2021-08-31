ecca7b5564aa27dcb56e6aa05d579c5b5ff785a5
ravi <ravikanthmp123@gmail.com>
2021-08-31 11:48:35 -0700

    Q146. LRU Cache

--- src/main/java/DSA/leetcode/Q287.java
+++ src/main/java/DSA/leetcode/Q287.java
@@ -1,21 +1,28 @@
 package DSA.leetcode;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 public class Q287 {
 
     public int findDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            if (seen.contains(num)) {
-                return num;
-            }
-            seen.add(num);
-        }
-        return -1;
+        int fast = 0;
+        int slow = fast;
+        do{
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }while (fast != slow);
+
+        int outsideCyclePtr = 0;
+        int insideCyclePtr = fast;
+        do{
+            insideCyclePtr = nums[insideCyclePtr];
+            outsideCyclePtr = nums[outsideCyclePtr];
+        }while (outsideCyclePtr != insideCyclePtr);
+
+        return outsideCyclePtr;
     }
 
-
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 2};
+        Q287 test = new Q287();
+        System.out.println(test.findDuplicate(arr));
+    }
 }
