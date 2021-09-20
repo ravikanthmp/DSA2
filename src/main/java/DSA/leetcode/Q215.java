b60655b5e6d15a58d8c6b6d7174d4d98d320fd9b
ravi <ravikanthmp123@gmail.com>
2021-09-20 09:30:20 -0700

    kth Largest..

--- src/main/java/DSA/leetcode/Q215.java
+++ src/main/java/DSA/leetcode/Q215.java
@@ -1,39 +1,51 @@
 package DSA.leetcode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Q215 {
 
     public int findKthLargest(int[] nums, int k) {
-        int[] afterSort = sort(nums);
-        return afterSort[nums.length - k];
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Comparator.reverseOrder());
+        for (int num : nums) {
+            pq.add(num);
+        }
+        int i = 1;
+        while (i != k){
+            pq.remove();
+            i++;
+        }
+        return pq.peek();
     }
 
-    private int[] sort(int[] nums) {
-
-        int shift = 10000;
-
-        // init
-        int[] count = new int[(2 * shift) + 1];
-
-        // freq
-        for (int i = 0; i < nums.length; i++) {
-            count[nums[i] + shift]++;
-        }
-
-        // accumulate
-        for (int i = 1; i < count.length; i++) {
-            count[i] += count[i - 1];
-        }
-
-        int[] temp = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            int idx = count[nums[i]];
-            temp[idx] = nums[i];
-            count[nums[i]] = idx + 1;
-        }
-
-        nums = temp;
-        return nums;
-    }
+//    private void sort(int[] nums) {
+//
+//        int shift = 10;
+//
+//        // init
+//        int[] count = new int[(2 * shift) + 1];
+//
+//        // freq
+//        for (int i = 0; i < nums.length; i++) {
+//            count[nums[i] + shift]++;
+//        }
+//
+//        // accumulate
+//        for (int i = 1; i < count.length; i++) {
+//            count[i] += count[i - 1];
+//        }
+//
+//        int[] temp = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            int idx = count[nums[i] + 1];
+//            temp[idx] = nums[i];
+//            count[nums[i]+1]++;
+//        }
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            nums[i] = temp[i];
+//        }
+//    }
 
     public static void main(String[] args) {
         int[] arr = {3,2,1,5,6,4};
