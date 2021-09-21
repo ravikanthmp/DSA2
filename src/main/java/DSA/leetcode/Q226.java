6eeda28222085760f5606ae3366f4dde336f02f5
ravi <ravikanthmp123@gmail.com>
2021-09-21 09:25:58 -0700

    invert tree with BFS

--- src/main/java/DSA/leetcode/Q226.java
+++ src/main/java/DSA/leetcode/Q226.java
@@ -1,18 +1,36 @@
 package DSA.leetcode;
 
 import DSA.practise.tree.binaryTree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Q226 {
 
     public TreeNode invertTree(TreeNode root) {
-        if(root == null){
-            return null;
-        }else{
-            TreeNode left = invertTree(root.left);
-            TreeNode right = invertTree(root.right);
-            root.right = left;
-            root.left = right;
-            return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root != null)queue.add(root);
+        while (!queue.isEmpty()){
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode first = queue.remove();
+                if (first.left != null){
+                    queue.add(first.left);
+                }
+                if (first.right != null){
+                    queue.add(first.right);
+                }
+                swapChildren(first);
+            }
+        }
+       return root;
+    }
+
+    private void swapChildren(TreeNode root){
+        if (root != null){
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
         }
     }
 }
