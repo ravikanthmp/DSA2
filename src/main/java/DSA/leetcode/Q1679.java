8038b89ae0946915755fb964a16363d718eb3b5a
ravi <ravikanthmp123@gmail.com>
2021-09-28 12:30:15 -0700

    TreeMap ... O(N) but 5%ile :(

--- src/main/java/DSA/leetcode/Q1679.java
+++ src/main/java/DSA/leetcode/Q1679.java
@@ -1,29 +1,40 @@
 package DSA.leetcode;
 
-import java.util.Arrays;
+import java.util.TreeMap;
 
 public class Q1679 {
 
     public int maxOperations(int[] nums, int k) {
 
-        Arrays.sort(nums);
+        // freq map
+        TreeMap<Integer, Integer> freqMap = new TreeMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
+        }
 
         int total = 0;
+        for (Integer x : freqMap.keySet()) {
+            int other = k - x;
+            if (other < x){
+                break;
+            }
 
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right){
-            int cmp = Integer.compare(nums[left] + nums[right], k);
-            if (cmp == 0){
-                total++;
-                left++;
-            }else if (cmp > 0){
-                right--;
+            if (!freqMap.containsKey(other)){
+                continue;
+            }
+            if (x == other){
+                total += freqMap.get(other)/2;
+                freqMap.put(x, 0);
             }else {
-                left++;
+                int min = Math.min(freqMap.get(x), freqMap.get(other));
+                total += min;
+                freqMap.put(x, freqMap.get(x) - min);
+                freqMap.put(other, freqMap.get(other) - min);
             }
         }
+
         return total;
+
     }
 
 }
