1f91d1f018c0410c9aed0572dbc0d9e32cb95224
ravi <ravikanthmp123@gmail.com>
2021-09-30 12:01:08 -0700

    blind 75 Arrays last

--- src/main/java/DSA/leetcode/Q33.java
+++ src/main/java/DSA/leetcode/Q33.java
@@ -1,70 +1,37 @@
 package DSA.leetcode;
 public class Q33 {
 
+    public int search(int[] nums, int target) {
 
-    public int findMinIndex(int[] nums) {
         int lo = 0;
         int hi = nums.length - 1;
+
         while (lo <= hi){
-
-            if (lo == hi){
-                return lo;
-            }else if (hi == lo + 1){
-                return nums[lo] < nums[hi] ? lo : hi;
-            }else {
-
-                int mid = lo + (hi - lo)/2;
-                if (nums[mid] < nums[mid - 1]){
-                    return mid;
-                }else if (nums[mid] > nums[mid + 1]){
-                    return mid + 1;
-                }
-
-                if (nums[mid] > nums[hi]){
+            int mid = lo + (hi - lo)/2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            if (nums[lo] < nums[hi]){
+                if (target > nums[mid]){
                     lo = mid + 1;
                 }else {
                     hi = mid - 1;
                 }
-
-            }
-
-
-
-        }
-        return -1;
-    }
-
-
-    public int search(int[] nums, int target) {
-        int minIndex = findMinIndex(nums);
-        if (minIndex < 0){
-            return -1;
-        }else {
-            int lo;
-            int hi;
-            if (target >= nums[minIndex] && target <= nums[nums.length - 1]){
-                lo = minIndex;
-                hi = nums.length - 1;
+            }else if (nums[mid] > nums[hi]){
+                if (target >= nums[lo] && target < nums[mid]){
+                    hi = mid - 1;
+                }else {
+                    lo = mid + 1;
+                }
             }else {
-                lo = 0;
-                hi = minIndex - 1;
+                if (target > nums[mid] && target < nums[hi]){
+                    lo = mid + 1;
+                }else {
+                    hi = mid - 1;
+                }
             }
-            return binarySearch(nums, lo, hi, target);
         }
-    }
 
-    private int binarySearch(int[] nums, int lo, int hi, int target) {
-        while (lo <= hi){
-            int mid = lo + (hi - lo)/2;
-            int cmp = Integer.compare(target, nums[mid]);
-            if (cmp == 0){
-                return mid;
-            }else if (cmp < 0){
-                hi = mid - 1;
-            }else {
-                lo = mid + 1;
-            }
-        }
         return -1;
     }
 
