5429561d4be3371afb2ef000603528b69db1150e
ravi <ravikanthmp123@gmail.com>
2021-10-07 19:56:17 -0700

    Q435 Tabulation still TLE

--- src/main/java/DSA/leetcode/Q435.java
+++ src/main/java/DSA/leetcode/Q435.java
@@ -2,79 +2,45 @@ package DSA.leetcode;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Q435 {
 
-    class Interval {
-        int start;
-        int end;
-
-        public Interval(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        public boolean intersects(Interval other) {
-            Interval sooner = (start <= other.start) ? this : other;
-            Interval later = (sooner == this) ? other : this;
-            return later.start < sooner.end;
-        }
-
-        public Interval merge(Interval other) {
-            return new Interval(Math.min(start, other.start), Math.max(end, other.end));
-        }
-    }
-
-    int[] memo;
-
     public int eraseOverlapIntervals(int[][] intervals) {
 
-        memo = new int[intervals.length];
+        int[] tab = new int[intervals.length];
+        tab[intervals.length - 1] = 1;
 
-        Interval[] intervalsArr = Arrays.stream(intervals)
-                .map(x -> new Interval(x[0], x[1]))
-                .toArray(Interval[]::new);
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
 
-        Arrays.sort(intervalsArr, Comparator.comparingInt(interval -> interval.start));
+        for (int i = intervals.length - 2; i >= 0; i--) {
 
-        int nonOverlapping =  maxNonOverLappingIntervals(0, intervalsArr);
+            int[] curr = intervals[i];
 
-        return intervals.length - nonOverlapping;
-    }
-
-    private int maxNonOverLappingIntervals(int idx, Interval[] intervalsArr){
-        if (idx == intervalsArr.length){
-            return 0;
-        }else {
-
-            if (memo[idx] == 0) {
-
-                Interval curr = intervalsArr[idx];
-                int ans = 1;
-                for (int i = idx + 1; i < intervalsArr.length; i++) {
-                    Interval other = intervalsArr[i];
-                    if (other.intersects(curr)){
-                        int x = maxNonOverLappingIntervals(i, intervalsArr);
-                        ans = Math.max(ans, x);
-                    }else {
-                        int x = 1 + maxNonOverLappingIntervals(i, intervalsArr);
-                        ans = Math.max(ans, x);
-                        break;
-                    }
+            int ans = 1;
+            int j;
+            for ( j = i + 1; j < intervals.length; j++) {
+                int[] interval = intervals[j];
+                if (interval[0] >= curr[1]){
+                    break;
+                }else {
+                    ans = Math.max(ans, tab[j]);
                 }
-
-                memo[idx] = ans;
             }
-
-            return memo[idx];
+            if (j != intervals.length){
+                tab[i] = Math.max(1 + tab[j], ans);
+            }else {
+                tab[i] = ans;
+            }
         }
+
+        return intervals.length - tab[0];
+
+
     }
 
     public static void main(String[] args) {
         Q435 test = new Q435();
-        int[][] intervals = {{1, 2}, {2, 3},{3, 4}, {1, 3}};
+        int[][] intervals = {{1, 100}, {11, 22},{1, 11}, {2, 12}};
         System.out.println(test.eraseOverlapIntervals(intervals));
     }
 }
