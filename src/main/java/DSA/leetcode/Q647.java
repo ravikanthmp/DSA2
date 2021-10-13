14df989c1aee26f270bddf73b566d1b65d3578be
ravi <ravikanthmp123@gmail.com>
2021-10-13 11:13:23 -0700

    Q647 Palindrome count

--- src/main/java/DSA/leetcode/Q647.java
+++ src/main/java/DSA/leetcode/Q647.java
@@ -1,28 +1,50 @@
 package DSA.leetcode;
 
 
+import java.util.Arrays;
+
 public class Q647 {
 
     public int countSubstrings(String s) {
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            res += palindromeCount(i, s);
+
+        if (s == null || s.isEmpty()){
+            return 0;
+        }else {
+            boolean[] prev = new boolean[s.length()];
+            boolean[] prevPrev = new boolean[s.length()];
+
+            // size 1
+            Arrays.fill(prevPrev, true);
+            int count = s.length();
+
+            // size 2
+            for (int i = 0; i < s.length() - 1; i++) {
+                prev[i] = (s.charAt(i) == s.charAt(i + 1));
+                if (prev[i]){
+                    count++;
+                }
+            }
+
+            for (int size = 3; size <= s.length(); size++) {
+
+                boolean[] temp = new boolean[s.length()];
+
+                for (int startIdx = 0; startIdx <= s.length() - size; startIdx++) {
+
+                    temp[startIdx] =
+                            ((s.charAt(startIdx) == s.charAt(startIdx + size - 1)) && prevPrev[startIdx + 1]);
+                    if (temp[startIdx]){
+                        count++;
+                    }
+                }
+
+                prevPrev = prev;
+                prev = temp;
+            }
+
+            return count;
         }
-        return res;
     }
-
-    private int palindromeCount(int i, String s) {
-        return check(i, i, s) + check(i, i + 1, s);
-    }
-
-    private int check(int left, int right, String s) {
-        int count = 0;
-        while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)){
-            count++;
-        }
-        return count;
-    }
-
 
     public static void main(String[] args) {
         Q647 test = new Q647();
