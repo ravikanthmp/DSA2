f259669352da565b3964f984b68786e092fa8d94
ravi <ravikanthmp123@gmail.com>
2021-10-15 09:59:01 -0700

    teamblind 75 last day..Word Search 2 and find Median in stream (do again)

--- src/main/java/DSA/leetcode/Q347.java
+++ src/main/java/DSA/leetcode/Q347.java
@@ -4,59 +4,26 @@ import java.util.*;
 
 public class Q347 {
 
-    class Pair{
-        int num;
-        int freq;
-
-        public Pair(int num, int freq) {
-            this.num = num;
-            this.freq = freq;
-        }
-    }
 
     public int[] topKFrequent(int[] nums, int k) {
-
-        /*
-        T(N) = O(NlogN) if k ~ N
-        S(N) = O(N)
-         */
-
-        // store frequency O(N)
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : nums){
-            frequencyMap.put(num, frequencyMap.getOrDefault(num , 0) + 1);
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0)  + 1);
         }
 
-        // top k O(N)
-        int i;
-        do {
-            i = quickSelect(nums, frequencyMap);
-        }while (i != k);
+        Comparator<Integer> cmp = (i, j) -> Integer.compare(freqMap.get(i), freqMap.get(j));
 
-        // O(klogN)
-       return Arrays.copyOf(nums, k);
-
-    }
-
-    private void exch(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private int quickSelect(int[] nums, Map<Integer, Integer> frequencyMap){
-        int pivot = nums[0];
-        int lt = 0;
-        int i = 0;
-        while (i < nums.length){
-            int cmp = Integer.compare(frequencyMap.get(pivot), frequencyMap.get(nums[i]));
-            if (cmp <= 0){
-                i++; lt++;
-            }else {
-                exch(nums, lt++, i++);
-            }
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(cmp.reversed());
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            maxPQ.add(entry.getKey());
         }
-        return lt;
+
+        int[] el = new int[k];
+        for (int i = 0; i < k; i++) {
+            el[i] = maxPQ.remove();
+        }
+        return el;
+
     }
 
     public static void main(String[] args) {
