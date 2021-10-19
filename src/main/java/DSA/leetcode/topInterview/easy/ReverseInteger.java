fb1baf313f5f78304edc4ba776888918a28c6c04
ravi <ravikanthmp123@gmail.com>
2021-10-19 09:07:37 -0700

    Reverse Integer

--- src/main/java/DSA/leetcode/topInterview/easy/ReverseInteger.java
+++ src/main/java/DSA/leetcode/topInterview/easy/ReverseInteger.java
@@ -1,2 +1,39 @@
-package DSA.leetcode.topInterview.easy;public class ReverseInteger {
+package DSA.leetcode.topInterview.easy;
+
+public class ReverseInteger {
+
+    public int reverse(int x)  {
+        boolean isNegative = (x < 0);
+        if (x == Integer.MIN_VALUE){
+            return 0;
+        }
+        System.out.println(Integer.MAX_VALUE);
+        x = Math.abs(x);
+       int currTotal = 0;
+       int limit = Integer.MAX_VALUE / 10;
+       while (x != 0){
+           int curr = Math.abs(x% 10);
+
+           if (currTotal == limit){
+               if (curr > Integer.MAX_VALUE % 10){
+                   return 0;
+               }else {
+                   currTotal = (currTotal * 10) + curr;
+               }
+           }else if (currTotal > limit){
+               return 0;
+           }else {
+               currTotal = (currTotal * 10) + curr;
+           }
+
+           x = x/10;
+       }
+
+       return isNegative ? -1 * currTotal : currTotal;
+    }
+
+    public static void main(String[] args) {
+        ReverseInteger reverseInteger = new ReverseInteger();
+        System.out.println(reverseInteger.reverse(-2147483648));
+    }
 }
--- src/main/java/DSA/leetcode/topInterview/easy/ValidSudoku.java
+++ src/main/java/DSA/leetcode/topInterview/easy/ValidSudoku.java
@@ -1,13 +1,8 @@
 package DSA.leetcode.topInterview.easy;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ValidSudoku {
 
     public boolean isValidSudoku(char[][] board) {
-
-
 
         for (int row = 0; row < board.length; row++) {
 
@@ -38,15 +33,17 @@ public class ValidSudoku {
     }
 
     private boolean isValid(char[][] board, int row, int col) {
-        Set<Character> seen = new HashSet<>();
+        int x = 0;
         for (int i = row; i < row + 3; i++) {
             for (int j = col; j < col + 3; j++) {
-                if (seen.contains(board[i][j])){
+                if (!Character.isDigit(board[i][j])){
+                    continue;
+                }
+                int y = board[i][j] - '0';
+                if ( (x & (1 << y)) != 0){
                     return false;
                 }
-                if (Character.isDigit(board[i][j])){
-                    seen.add(board[i][j]);
-                }
+                x = x | (1 << y);
 
             }
         }
@@ -54,29 +51,32 @@ public class ValidSudoku {
     }
 
     private boolean checkColumn(char[][] board, int col) {
-
-        Set<Character> seen = new HashSet<>();
+        int x = 0;
         for (int row = 0; row < board.length; row++) {
-            if (seen.contains(board[row][col])){
+            if (!Character.isDigit(board[row][col])){
+                continue;
+            }
+            int y = board[row][col] - '0';
+            if ( (x & (1 << y)) != 0){
                 return false;
             }
-            if (Character.isDigit(board[row][col])){
-                seen.add(board[row][col]);
-            }
+            x = x | (1 << y);
 
         }
         return true;
     }
 
     private boolean checkRow(char[][] board, int row) {
-        Set<Character> seen = new HashSet<>();
+        int x = 0;
         for (int col = 0; col < board[0].length; col++) {
-            if (seen.contains(board[row][col])){
+            if (!Character.isDigit(board[row][col])){
+                continue;
+            }
+            int y = board[row][col] - '0';
+            if ( (x & (1 << y)) != 0){
                 return false;
             }
-            if (Character.isDigit(board[row][col])){
-                seen.add(board[row][col]);
-            }
+            x = x | (1 << y);
         }
         return true;
 
